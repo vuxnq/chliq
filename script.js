9aa2595780de6978a26cb9af9232dfa886d530e1
vuxnq <vuxnqu@gmail.com>
2022-05-23 21:45:41 +0200

    added shop (not functioning assistant)

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-var score = 0
 const texts = ["i ain't lied to nobody but me",
   "where were you when i needed you",
   "what if the night keeps falling",
@@ -8,21 +7,56 @@ const texts = ["i ain't lied to nobody but me",
   "cocaine for breakfast",
   "medusa never turned me to a statue",
   "i saw the sparks",
-  "my regrets look just like texts i shouldn't send",];
+  "my regrets look just like texts i shouldn't send",
+  "wash it out of all of me to feel the fire",];
 
+var score = 0;
+var click = 1;
+var assistant = 0;
+
+var clickPrice = 10;
+var assistantPrice = 1000;
 
 document.getElementById("chliq").addEventListener("click", chliq);
+document.getElementById("click-upgrade").addEventListener("click", clickUpgrade);
+document.getElementById("assistant-upgrade").addEventListener("click", assistantUpgrade);
+
+function updater() {
+  document.title = "chliq | " + score;
+  document.getElementById("click-upgrade").getElementsByClassName("counter")[0].innerHTML = click;
+  document.getElementById("click-upgrade").getElementsByClassName("price")[0].innerHTML = clickPrice;
+  document.getElementById("assistant-upgrade").getElementsByClassName("counter")[0].innerHTML = assistant;
+  document.getElementById("assistant-upgrade").getElementsByClassName("price")[0].innerHTML = assistantPrice;
+  milestone();
+}
+
+function milestone() {
+  if (score >= 10) {
+    document.getElementById("shop").style.display = "block";
+  }
+}
 
 function chliq() {
-  score++;
-  document.title = "chliq | " + score;
+  score += click;
   let random = texts[Math.floor((Math.random() * texts.length))];
   document.getElementById("background-text").innerHTML = random;
   updater();
 }
 
-function updater() {
-  if (score >= 10) {
-    document.getElementById("shop").style.display = "block";
+function clickUpgrade() {
+  if (score >= clickPrice) {
+    click++;
+    score -= clickPrice;
+    clickPrice = Math.round(clickPrice * (1 + (click / 10)));
   }
+  updater();
+}
+
+function assistantUpgrade() {
+  if (score >= assistantPrice) {
+    assistant++;
+    score -= assistantPrice;
+    assistantPrice = Math.round(assistantPrice * (1 + (assistant / 10)));
+  }
+  updater();
 }
